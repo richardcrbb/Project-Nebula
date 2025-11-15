c650839c440d58f2ca5af554df96c90c16be388f
Cristian Beltran <ricardocrbb@hotmail.com>
2025-11-15 18:08:35 -0500

    rutas relativas8

--- docs/script.js
+++ docs/script.js
@@ -1,20 +1,18 @@
 fetch('/Project-Nebula/components/sidebar.html', { cache: "no-store" })
-  .then(r => r.text())
+  .then(r => {
+    if (!r.ok) throw new Error('Sidebar not found: ' + r.status);
+    return r.text();
+  })
   .then(html => {
-    // Insertar HTML del sidebar
     const sidebarContainer = document.querySelector('#sidebar');
     sidebarContainer.innerHTML = html;
 
-    // AHORA sí existen .toggler y los elementos del sidebar
     const sidebar = sidebarContainer.querySelector("aside.sidebar");
     const sidebarToggler = sidebar.querySelector(".toggler");
 
-    // Funcion para escuchar clicks en el boton y agregar o quitar clase collapsed en elementos
     sidebarToggler.addEventListener("click", () => {
-        sidebar.classList.toggle("collapsed");
-        document.body.classList.toggle("sidebar-collapsed");
+      sidebar.classList.toggle("collapsed");
+      document.body.classList.toggle("sidebar-collapsed");
     });
-
-    
-
-  });
+  })
+  .catch(e => console.error(e));
