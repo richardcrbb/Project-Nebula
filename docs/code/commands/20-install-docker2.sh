490c1589f09a19be2159be192d875ad8448718aa
Cristian Beltran <ricardocrbb@hotmail.com>
2025-11-27 17:28:57 -0500

    docker modi

--- docs/code/commands/20-install-docker2.sh
+++ docs/code/commands/20-install-docker2.sh
@@ -1,10 +1,8 @@
 # Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+sudo sh -c 'cat > /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
-Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Suites: '$(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")'
 Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
-EOF
-
-sudo apt update
+EOF'
