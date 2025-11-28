b2264086fb3e6218bf0d324a88b170ad2cc3adb8
Cristian Beltran <ricardocrbb@hotmail.com>
2025-11-27 19:42:11 -0500

    docker modification

--- docs/code/commands/21-docker-test.sh
+++ docs/code/commands/21-docker-test.sh
@@ -1,1 +1,7 @@
+#The Docker service starts automatically after installation. To verify that Docker is running, use:
+
+sudo systemctl status docker
+
+# run the hello-world image
+
 sudo docker run hello-world
